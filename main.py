2c648e6710cff3c6eac94f9dc1ec393108fb0c79
Jamiel Abed <jamielabed12@gmail.com>
2019-07-22 15:25:21 -0500

    importing yelp api

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from google.appengine.api import users
 from google.appengine.api import urlfetch
 import os
 import json
+from yelp.client import Client
+
+MY_API_KEY = "cXFG1vvpqbRy7gQvhqKcbklCku8oq5AhVf5_goxfJ74qz6LcIAqB9fvzx7nZZI92ChAMHJ_02aQ923Q55Zstp8pfKZ4IYDE6iStAkPAF1PtOZkvCQq9Rx-W-hxU2XXYx"
+
+client = Client(MY_API_KEY)
+
+business_response = client.business.get_by_id('yelp-san-francisco')
 
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
