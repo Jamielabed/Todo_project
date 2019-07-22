592170563e4dfdb31a339a531625b691ab67f0dd
Jamiel Abed <jamielabed12@gmail.com>
2019-07-22 14:17:54 -0500

    changed link, made a template folder, made an html file

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self): #for a get request
         self.response.headers['Content-Type'] = 'text/html'
-        index_template = JINJA_ENV.get_template('templates/quiz.html')
+        index_template = JINJA_ENV.get_template('templates/main.html')
