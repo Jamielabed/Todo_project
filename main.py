f51bdbfdd75acb3f8039cf1763af7911e6718491
julian esparza <julianesparza35@gmail.com>
2019-07-22 15:22:38 -0700

    html corrrections template

--- main.py
+++ main.py
@@ -9,11 +9,30 @@ JINJA_ENV = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+
 class MainPage(webapp2.RequestHandler):
-    def get(self): #for a get request
+    def get(self):
+        user = users.get_current_user()
+        template = JINJA_ENV.get_template('templates/main.html')
+        data = {
+                 'user': user,
+                 'login_url': users.create_login_url(self.request.uri),
+                 'logout_url': users.create_logout_url(self.request.uri),
+                }
         self.response.headers['Content-Type'] = 'text/html'
-        index_template = JINJA_ENV.get_template('templates/main.html')
+        self.response.write(template.render(data))
+
+class AddInterestPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENV.get_template('templates/InterestPage.html')
+        data = { }
+
+
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(template.render(data))
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/', MainPage),
+    ('/AddInterest',AddInterestPage)
 ], debug=True)
