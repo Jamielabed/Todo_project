008ce47a4d8eaa60a53a860afa90a3b966dc538d
Jamiel Abed <jamielabed12@gmail.com>
2019-07-22 14:12:24 -0500

    attempt at a main handler

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class MainPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        self.response.headers['Content-Type'] = 'text/html'
+        index_template = JINJA_ENV.get_template('templates/quiz.html')
