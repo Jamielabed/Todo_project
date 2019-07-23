39ad70cafcd40ad1883fc56c470de7799dd36997
julian esparza <julianesparza35@gmail.com>
2019-07-23 11:26:47 -0700

    fixed input function on intrest page

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 from google.appengine.api import users
-from google.appengine.api import urlfetch
-import os
-import json
-
-YELP_API_KEY = "cXFG1vvpqbRy7gQvhqKcbklCku8oq5AhVf5_goxfJ74qz6LcIAqB9fvzx7nZZI92ChAMHJ_02aQ923Q55Zstp8pfKZ4IYDE6iStAkPAF1PtOZkvCQq9Rx-W-hxU2XXYx"
-
-
 
 JINJA_ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def root_parent():
+
+    return ndb.Key('Parent', 'default_parent')
+
+
+
+class Interest(ndb.Model):
+    interests = ndb.StringProperty()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -32,81 +33,50 @@ class MainPage(webapp2.RequestHandler):
 class AddInterestPage(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENV.get_template('templates/InterestPage.html')
-        data = { }
+        data = {
+        'Interests': Interest.query(ancestor=root_parent()).fetch()
+        }
 
 
         self.response.headers['Content-Type'] = 'text/html'
         self.response.write(template.render(data))
+        print('fine')
+    def post(self):
 
-
-class searchResults(webapp2.RequestHandler):
-    def get(self): #for a get request
+        template = JINJA_ENV.get_template('templates/InterestPage.html')
+        # data = {
+        # 'Interests': Interest.query(ancestor=root_parent()).fetch(),
+        # 'AddInterest':  Interest.query(ancestor=root_parent()).fetch()
+        # }
+        # print(data['Interests'])
+        # new_food = Interest(parent=root_parent())
+        # print(new_food)
+        # new_food.name = self.request.get('new_intrest')
+        # print(new_food.name)
+        # data['Interests'].append(new_food)
+        # print(data['Interests'])
+        new_interest = Interest(parent = root_parent())
+        added = self.request.get('new_interest')
+        new_interest.interests = added
+        new_interest.put()
         self.response.headers['Content-Type'] = 'text/html'
-        index_template = JINJA_ENV.get_template('templates/main.html')
-        try:
-            #form_data = {'location': 'Chicago'}
-            headers = {'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Bearer '+YELP_API_KEY}
+        self.redirect('/AddInterest')
+        print(new_interest)
 
-            result = urlfetch.fetch(
-                #payload = form_data,
-                method=urlfetch.GET,
-                url = "https://api.yelp.com/v3/businesses/search?location=Chicago",
-                headers=headers)
-            self.response.write(result.content)
-        except urlfetch.Error:
-            logging.exception('Caught exception fetching url')
+class DeleteInterests(webapp2.RequestHandler):
+    def post(self):
+        to_delete = self.request.get('to_delete', allow_multiple=True)
+        for entry in to_delete:
+            key = ndb.Key(urlsafe=entry)
+            key.delete()
+        # redirect to '/' so that the MainPage.get() handler will run and show
+        # the list of dogs.
+        self.redirect('/AddInterest')
 
-
-def get_current_location():
-    print "in current location"
-    #def post(self):
-    print "in post"
-    headers = {'Content-Type': 'application/json'}
-    result = urlfetch.fetch(
-             url="https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAfFZHWxBjkkd8vi12mY4d3IOaDHdBkuWE",
-             method=urlfetch.POST,
-             headers=headers)
-    return result.content
-def get_dist_matrix(curLoc, rest):
-    latitude = curLoc["location"]["lat"]
-    longitude = curLoc["location"]["lng"]
-    distMatrixURL = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + str(latitude) + "," + str(longitude) + "&destinations=" + rest["name"] + "&key=AIzaSyAfFZHWxBjkkd8vi12mY4d3IOaDHdBkuWE"
-    headers = {'Content-Type': 'application/json'}
-    result = urlfetch.fetch(
-             url=distMatrixURL,
-             method=urlfetch.POST,
-             headers=headers)
-    return json.loads(result.content)
-
-class MapsPage(webapp2.RequestHandler):
-    API_KEY = "AIzaSyAfFZHWxBjkkd8vi12mY4d3IOaDHdBkuWE"
-    def get(self):
-        #currentLocation = get_current_location()
-        currentLocation = json.loads(get_current_location())
-        #self.response.write(currentLocation)
-        print "in get"
-
-        chicago = {
-            "name": "Chicago"
-        }
-        seattle = {
-            "name": "Seattle"
-        }
-        restaurantsList = [chicago, seattle]
-        for restaurant in restaurantsList:
-            # separate words in destination should be separated w + (San+Francisco) --> not implemented, see if affects output
-            # ^^ put into lat/long so shouldn't be an issue
-            distMatrix = get_dist_matrix(currentLocation,restaurant)
-            restaurant["distance"] = distMatrix['rows'][0]['elements'][0]['distance']['text']
-            restaurant["duration"] = distMatrix['rows'][0]['elements'][0]['duration']['text']
-            print restaurant
-            print restaurant['duration']
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/AddInterest',AddInterestPage),
-    ('/searchResults', searchResults),
-    ('/maps', MapsPage)
+    ('/AddInterest', AddInterestPage),
+    ('/DeleteInterests', DeleteInterests)
 ], debug=True)
