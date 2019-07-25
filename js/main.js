2381a685c7a393957116fb1633a0a303f6989887
Natalie Abreu <natalie.abreu3@gmail.com>
2019-07-25 14:35:32 -0500

    Revert "favorites button formatting"
    This reverts commit 12d1f01e1d80606f11509704ef10fab120533f07.

--- js/main.js
+++ js/main.js
@@ -23,13 +23,15 @@ function formatBoxes() {
 //   })
 //   console.log("IN JAVASCRIPT MAIN")
 // }
-}
+
 formatBoxes()
 
-
+let sortChoice = document.querySelector('#sort_selection')
 // sortChoice.addEventListener("submit", includeSortBy(latitude,longitude));
 //
 // function includeSortBy(lat,long) {
 //
 //   console.log("IN SORT BY FUNCTION")
 //   window.location.href = `/?lat=${latitude}&long=${longitude}&sort_selection=${sortChoice}`
+
+//}
